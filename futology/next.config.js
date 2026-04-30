7bb995198192b92d5ecf5c95aa239ed2626ce325
Sonik Krish <sonikrish2248@gmail.com>
2026-04-30 15:59:56 +0530

    chore(futology): allow image hosts (api-sports, football-data, transfermarkt)

---
+++ futology/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "media.api-sports.io" },
+      { protocol: "https", hostname: "crests.football-data.org" },
+      { protocol: "https", hostname: "img.a.transfermarkt.technology" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
